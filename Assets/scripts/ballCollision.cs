aaaed8f90501805401c8a387b3cde26afa1b3a5c
codyCIM2 <c0dysmyth@gmail.com>
2020-03-10 17:02:40 -0400

    finalFromCIM
    finalFromCIM

---
+++ Assets/scripts/ballCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ballCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "green")
+        {
+            ScoreManager.Instance.UpdateScore(10);
+
+        }
+        if (col.gameObject.tag == "red")
+        {
+            ScoreManager.Instance.UpdateScore(-7);
+
+        }
+    }
+}
